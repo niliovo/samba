c0790a612207297a637b5d54395d88a6517c46ff
nili <1791356563@qq.com>
2023-08-16 17:44:52 +0800

    update

--- dockerfile/samba_build_armv7.sh
+++ dockerfile/samba_build_armv7.sh
@@ -2,7 +2,7 @@
 
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories &&
 
-apk --update --no-cache add bash coreutils jq samba shadow tzdata yq
+apk --update --no-cache add bash coreutils jq samba shadow tzdata yq bash
 
 cp /usr/share/zoneinfo/${TZ} /etc/localtime
 echo ${TZ} > /etc/timezone
